0b9e15994907de09d985c9eab1d00c477d4a57cf
PraveenNageswaran <nageswaranpraveen@gmail.com>
2020-11-03 04:42:08 +0000

    slack

--- mainRun/run.groovy
+++ mainRun/run.groovy
@@ -7,5 +7,10 @@ pipeline {
             }
             
         }
+        stage('Slack integration') {
+            steps {
+                slackSend channel: 'spring', color: 'blue', message: 'welcome', teamDomain: 'prashanti', tokenCredentialId: 'Slack', username: 'Jenkins'
+            }
+        }
     }
 }
